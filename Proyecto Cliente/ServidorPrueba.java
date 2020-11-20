1d07b78ecca8090ec3299ba23f7208af855ab1d5
Joaquin Marroquin <68405995+JAL0NS0@users.noreply.github.com>
2020-11-19 21:19:12 -0600

    Add files via upload
    Agregamos la clase DB para manejo de base de datos y un archivo de prueba

--- Proyecto Cliente/ServidorPrueba.java
+++ Proyecto Cliente/ServidorPrueba.java
@@ -16,10 +16,14 @@ public class ServidorPrueba {
         usuarios.put("joaquin", "alonso19");
         servidores.put("oscar@", "127.0.0.2");
         servidores.put("rodrigo@", "127.0.0.3");
+        servidores.put("andres@","127.9.9.9");
+        servidores.put("juan@","179.2.0.0");
 
         try {
-            ServerSocket server = new ServerSocket(port);
-            System.out.println("Escuchando conexion por el puerto " + port + " ...");
+          ServerSocket server = new ServerSocket(port);
+          System.out.println("Escuchando conexion por el puerto " + port + " ...");
+          ciclo:
+          while(true){
             Socket socketClient = server.accept();
             System.out.println("Conexion aceptada!");
 
@@ -32,91 +36,101 @@ public class ServidorPrueba {
             //out.println("Bienvenido al servidor de cc2 :)");
 
             while(true){
-                String usa = in.readLine();
-                if(usa != null){
-                    System.out.println("Cliente: " + usa);
-                    String[] flag = usa.split(" ");
-                    if((flag[0]).equals("LOGIN")){
-                        if(usuarios.containsKey(flag[1])){
-                           if(usuarios.get(flag[1]).equals(flag[2])){
-                                System.out.println("Servidor: OK LOGIN");
-                                out.println("OK LOGIN");
-                            } else{
-                                out.println("ERROR: 102");
-                                System.out.println("LOGIN ERROR 102");
-                            }
-
-                      }else {
-                            out.println("ERROR: 101");
-                            System.out.println("LOGIN ERROR 101");
-                        }
-
-                    } else if((flag[0].equals("CLIST"))){
-                        if(usuarios.containsKey(flag[1])){
-                            int contador = 0;
-                            for(String i : servidores.keySet()){
-                                contador++;
-                                if(contador != servidores.size()){
-                                    System.out.println("Servidor: OK CLIST " + i + servidores.get(i));
-                                    out.println("OK CLIST " + i + servidores.get(i));
-                                } else {
-                                    System.out.println("Servidor: OK CLIST " + i + servidores.get(i) + " *");
-                                    out.println("OK CLIST " + i + servidores.get(i) + " *");
-                                }
-                            }
-                        }
-                        //}
-                    } else if((flag[0].equals("GETNEWMAILS"))){
-                        if(usuarios.containsKey(flag[1])){
-                            int contador = 0;
-                            for(String i : servidores.keySet()){
-                                contador++;
-                                if(contador != servidores.size()){
-                                    System.out.println("Servidor: OK GETNEWMAILS " + i + servidores.get(i) + " \"Hola, como estas\"  \"como vendras\" ") ;
-                                    out.println("OK GETNEWMAILS " + i + servidores.get(i) + " \"Hola, como estas\"  \"como vendras\" ");
-                                } else {
-                                    System.out.println("Servidor: OK GETNEWMAILS " + i + servidores.get(i) + " \"Hola, como estas\"  \"como vendras\" " +  " *");
-                                    out.println("OK CLIST " + i + servidores.get(i) + " \"Hola, como estas\"  \"como vendras\" " + " *");
-                                }
-                            }
-                        }
-                    }else if((flag[0].equals("LOGOUT"))){
-                      out.println("OK LOGOUT");
-                      System.out.println("Servidor: OK LOGOUT");
-                      break;
-                    }else if((flag[0].equals("SEND"))){
-                      ArrayList<String> contactos= new ArrayList<String>();
-                      String contacto;
-                      while(true){
-                        contacto=in.readLine();
-                        contactos.add(contacto);
-                        System.out.println(contacto);
-                        if(contacto.charAt(contacto.length()-1) == '*'){
-                            System.out.println("Recibido ultimo contacto");
-                            break;
-                        }
-                      }
-                      String asunto= in.readLine();
-                      String cuerpo= in.readLine();
-                      String enviar= in.readLine();
-                      System.out.println("Servidor: OK SEND MAIL");
-                      out.println("OK SEND MAIL");
-
-                    }else if ((flag[0].equals("NEWCONT"))){
-                      String contacto=flag[1];
-                      System.out.println(contacto);
-                      out.println("OK NEWCONT "+contacto);
-                    }else{
-                      System.out.println("INVALID COMMAND ERROR");
+              String usa = in.readLine();
+              if(usa != null){
+                System.out.println("Cliente: " + usa);
+                String[] flag = usa.split(" ");
+                if((flag[0]).equals("LOGIN")){
+                  System.out.println(flag[1]);
+                  if(usuarios.containsKey(flag[1])){
+                    if(usuarios.get(flag[1]).equals(flag[2])){
+                      System.out.println("Servidor: OK LOGIN");
+                      out.println("OK LOGIN");
+                    } else{
+                      out.println(Comandos.LOGIN_ERROR+"102");
+                      System.out.println(Comandos.LOGIN_ERROR+"102");
+                      in.close();
+                      out.close();
+                      socketClient.close();
+                      continue ciclo;
                     }
+
+                  }else {
+                    out.println(Comandos.LOGIN_ERROR+"101");
+                    System.out.println(Comandos.LOGIN_ERROR+"101");
+                    in.close();
+                    out.close();
+                    socketClient.close();
+                    continue ciclo;
+                  }
+
+                } else if((flag[0].equals("CLIST"))){
+                  if(usuarios.containsKey(flag[1])){
+                    int contador = 0;
+                    for(String i : servidores.keySet()){
+                      contador++;
+                      if(contador != servidores.size()){
+                        System.out.println("Servidor: OK CLIST " + i + servidores.get(i));
+                        out.println("OK CLIST " + i + servidores.get(i));
+                      } else {
+                        System.out.println("Servidor: OK CLIST " + i + servidores.get(i) + " *");
+                        out.println("OK CLIST " + i + servidores.get(i) + " *");
+                      }
+                      Thread.sleep(10);
+                    }
+                  }
+                  //}
+                } else if((flag[0].equals("GETNEWMAILS"))){
+                  if(usuarios.containsKey(flag[1])){
+                    int contador = 0;
+                    for(String i : servidores.keySet()){
+                      contador++;
+                      if(contador != servidores.size()){
+                        System.out.println("Servidor: OK GETNEWMAILS " + i + servidores.get(i) + " \"Hola\" \"Como estas,como vendras\" ") ;
+                        out.println("OK GETNEWMAILS " + i + servidores.get(i) + " \"Hola\" \"Como estas,como vendras\" ");
+                      } else {
+                        System.out.println("Servidor: OK GETNEWMAILS " + i + servidores.get(i) + " \"Buenas\" \"Mi nombre es Pepe\" " +  " *");
+                        out.println("OK GETNEWMAILS " + i + servidores.get(i) + " \"Buenas\" \"Mi nombre es Pepe\" " + " *");
+                      }
+                      Thread.sleep(10);
+                    }
+                  }
+                }else if((flag[0].equals("LOGOUT"))){
+                  out.println("OK LOGOUT");
+                  System.out.println("Servidor: OK LOGOUT");
+                  in.close();
+                  out.close();
+                  socketClient.close();
+                  continue ciclo;
+                }else if((flag[0].equals("SEND"))){
+                  ArrayList<String> contactos= new ArrayList<String>();
+                  String contacto;
+                  while(true){
+                    contacto=in.readLine();
+                    contactos.add(contacto);
+                    System.out.println(contacto);
+                    if(contacto.charAt(contacto.length()-1) == '*'){
+                      System.out.println("Recibido ultimo contacto");
+                      break;
+                    }
+                  }
+                  String asunto= in.readLine();
+                  String cuerpo= in.readLine();
+                  String enviar= in.readLine();
+                  System.out.println("Servidor: OK SEND MAIL");
+                  out.println("OK SEND MAIL");
+
+                }else if ((flag[0].equals("NEWCONT"))){
+                  String contacto=flag[1];
+                  System.out.println(contacto);
+                  out.println("OK NEWCONT "+contacto);
+                }else{
+                  System.out.println("INVALID COMMAND ERROR");
                 }
+              }
 
             }
-
-
-            in.close();
-            out.close();
-            socketClient.close();
+          }
         } catch (Exception e) {
             e.printStackTrace();
         }
